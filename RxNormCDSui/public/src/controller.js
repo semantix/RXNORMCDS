0f3b839423678ec1c34792e89ae27cfb73d1abb4
Deepak Sharma <sharma.deepak2@mayo.edu>
2015-04-04 19:37:47 -0500

    Added elements to UI for review

--- RxNormCDSui/public/src/controller.js
+++ RxNormCDSui/public/src/controller.js
@@ -2,6 +2,49 @@ angular.module('RxNormReport')
 .controller('RxNormReview', function ($scope) {
 	$scope.mainReviewers = ['Kelly A', 'Robert B', 'Anne C'];
 	$scope.guestReviewers = ['Guest Reviewer'];
+
+	$scope.propertyList = ['Dose Form: Drug Form',
+							'Dose Form: Release Pattern',
+							'Dose Form: Delivery Form',
+							'Dose Form: ER Time',
+							'Administration: Route',
+							'Administration: Method',
+							'Delivery Device: Type',
+							'Delivery Device: Capacity',
+							'Delivery Device: ER Time'];
+
+	function exitingValues () {
+    	this.rxndf = "rxndf_value_here";
+    	this.rxtndf = "rxtndf_value_here";
+    	this.rxtroute= "rxtroute_value";
+	};
+
+	function proposedValues () {
+    	this.dfdrugform = "DF_Drug_Form_here";
+    	this.dfreleasepattern = "DF_Release_Pattern_here";
+    	this.dfdeliveryform= "DF_Delivery_Form_here";
+    	this.dfertime= "DF_ER_time_here";
+
+    	this.admroute= "ADMIN_Route_here";
+    	this.admmethod= "ADMIN_Method_here";
+
+    	this.ddtype= "DEL_Type_here";
+    	this.ddcapacity= "DEL_Capacity_here";
+    	this.ddertime= "DEL_ER_Time_here";
+	};
+
+	var drug = {
+    	scd : "exst_scd_value", 
+		rxcui : "rxcui_value",
+   		getExistingValues: function(){
+   			return new exitingValues();
+   		},
+   		getProposedValues: function(){
+   			return new proposedValues();
+   		}
+   	}
+
+   	$scope.currentDrug = drug;
 })
 .controller('RxNormList', function ($scope, MyList, $location) {
 	$scope.list1 = MyList.query();
--- RxNormCDSui/public/src/review.js
+++ RxNormCDSui/public/src/review.js
@@ -1,7 +1,41 @@
 var currentReviewer = "";
+var selectedProperty ="";
 
 function changeReviewer()
 {
 	currentReviewer = document.getElementById("reviewers").value;
 	console.log("Current Reviewer:" + currentReviewer);
+	document.getElementById("currentReviewer").innerHTML = currentReviewer;
+}
+
+function changeSelectedProperty()
+{
+	selectedProperty = document.getElementById("selectedProperty").value;
+	console.log("Property selected to comment:" + selectedProperty);
+}
+
+function setSelectedProperty(clickedProperty)
+{
+	document.getElementById("selectedProperty").value = clickedProperty;
+}
+
+function setOK()
+{
+	var elems = document.getElementsByTagName("span");
+
+	for (var i = 0; i<elems.length; i++) {
+
+		if (elems[i].class.name == ' glyphicon glyphicon-thumbs-up')
+		{
+        	if (elems[i].style.visibility == 'visible') {
+            	elems[i].style.visibility = 'hidden';    
+        	}
+        	else {
+            	elems[i].style.visibility = 'visible';
+        	}
+        }
+
+        //if (elems[i].style.color == 'blue')
+        //	elems[i].style.color = 'green';
+    }
 }
