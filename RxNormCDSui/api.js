a3ab766f05e8901c453bc4155a7bab812eacee0f
Deepak Sharma <sharma.deepak2@mayo.edu>
2015-04-27 12:07:39 -0500

    Added reconnect code to api.js

--- RxNormCDSui/api.js
+++ RxNormCDSui/api.js
@@ -12,7 +12,55 @@ var connection1 = mysql.createConnection({
                 database : 'rxnormcds',
         });
 
-connection1.connect();
+/**
+ * Setup a client to automatically replace itself if it is disconnected.
+ *
+ * @param {Connection} client
+ *   A MySQL connection instance.
+ */
+function replaceClientOnDisconnect(client) {
+  connection1.on("error", function (err) {
+    if (!err.fatal) {
+      return;
+    }
+ 
+    if (err.code !== "PROTOCOL_CONNECTION_LOST") {
+      throw err;
+    }
+ 
+    // client.config is actually a ConnectionConfig instance, not the original
+    // configuration. For most situations this is fine, but if you are doing
+    // something more advanced with your connection configuration, then
+    // you should check carefully as to whether this is actually going to do
+    // what you think it should do.
+    connection1 = mysql.createConnection(connection1.config);
+    replaceClientOnDisconnect(connection1);
+    connection1.connect(function (error) {
+      if (error) {
+        // Well, we tried. The database has probably fallen over.
+        // That's fairly fatal for most applications, so we might as
+        // call it a day and go home.
+        //
+        // For a real application something more sophisticated is
+        // probably required here.
+        process.exit(1);
+      }
+    });
+  });
+}
+ 
+// And run this on every connection as soon as it is created.
+replaceClientOnDisconnect(connection1);
+ 
+/**
+ * Every operation requiring a client should call this function, and not
+ * hold on to the resulting client reference.
+ *
+ * @return {Connection}
+ */
+exports.getClient = function () {
+  return connection1;
+};
 
 
 router
@@ -23,7 +71,7 @@ router
         var queryStr = 'select a.SCD_rxcui, a.review_status, b.DF_str ' +
                           ' from scd_review a, scd_df b ' +
                           ' where a.SCD_rxcui = b.SCD_rxcui and ' +
-                          ' a.review_status = "Incomplete" and' +
+                          ' ((a.review_status = "Incomplete")||(a.review_status = "In Progress")) and' +
                           ' a.review_priority < 100 ' +
                           ' order by a.review_priority, a.SCD_rxcui limit 25';
 
